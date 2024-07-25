e531fed18d3e50747c9ae7bf1f564dfab255868d
Bryanmankind <shegeemankind@gmail.com>
2024-07-25 03:03:24 +0100

    building page

--- src/Body.js
+++ src/Body.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import Logo from "./image/Logo.png";
-import DIVINE_3 from "./image/DIVINE_3.jpg";
-import DIVINE_1 from "./image/DIVINE_1.jpg";
-import DIVINE_2 from "./image/DIVINE_2.jpg";
-
 
 export function Body () {
     return (
       <div class="container">
           <div id="imgs">
-              <img class="active-0 active" src={Logo} alt='log'/>
-              <img class="active-1 hidden" src={DIVINE_3} alt='logo'/>
-              <img class="active-2 hidden" src={DIVINE_1} alt='log'/>
-              <img class="active-3 hidden" src={DIVINE_2} alt='log'/>
+              <img class="active-0 active" src="https://cdn.pixabay.com/photo/2022/08/31/19/10/ukraine-7424066_1280.png" alt='log'/>
           </div>
   
           <div class="welcome">
-              <h1>WELCOME TO <span>CHURCH</span></h1>
+              <h1>WELCOME TO <span>FAMILY</span></h1>
               <p>Welcome to Divne Mercy Seat Assembly. <br/> We love you and we're glad you made it here.</p>
           </div>
       </div>
