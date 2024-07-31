59b4eb5a8aef8d9f5351c477fce387eb3b51386d
Bryanmankind <shegeemankind@gmail.com>
2024-07-31 05:43:20 +0100

    backend

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+router.post('/prayer-request', async (req, res) => {
+    const { user_email, prayer_request } = req.body;
+    const newPrayerRequest = new PrayerRequest({ user_email, prayer_request });
+    try {
+      await newPrayerRequest.save();
+      res.status(201).send('Prayer request saved');
+    } catch (error) {
+      res.status(400).send('Error saving prayer request');
+    }
+  });
+  
+router.post('/plan-to-visit', async (req, res) => {
+    const { user_name, user_email, plan_to_visit } = req.body;
+    const newPlanToVisit = new PlanToVisit({ user_name, user_email, plan_to_visit });
+    try {
+      await newPlanToVisit.save();
+      res.status(201).send('Plan to visit saved');
+    } catch (error) {
+      res.status(400).send('Error saving plan to visit');
+    }
+  });
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require("./routes/router")
 const cors = require('cors'); 
+require("dotenv/config");
 
 const app = express();
 
@@ -21,7 +22,8 @@ app.use("/", router);
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/dmsacollection', { useNewUrlParser: true, useUnifiedTopology: true });
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(process.env.DB_URI, dbOptions);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -45,27 +47,7 @@ const PrayerRequest = mongoose.model('PrayerRequest', prayerRequestSchema);
 const PlanToVisit = mongoose.model('PlanToVisit', planToVisitSchema);
 
 // Routes
-app.post('/prayer-request', async (req, res) => {
-  const { user_email, prayer_request } = req.body;
-  const newPrayerRequest = new PrayerRequest({ user_email, prayer_request });
-  try {
-    await newPrayerRequest.save();
-    res.status(201).send('Prayer request saved');
-  } catch (error) {
-    res.status(400).send('Error saving prayer request');
-  }
-});
 
-app.post('/plan-to-visit', async (req, res) => {
-  const { user_name, user_email, plan_to_visit } = req.body;
-  const newPlanToVisit = new PlanToVisit({ user_name, user_email, plan_to_visit });
-  try {
-    await newPlanToVisit.save();
-    res.status(201).send('Plan to visit saved');
-  } catch (error) {
-    res.status(400).send('Error saving plan to visit');
-  }
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
