d6eb8bbef28efd4f2eababdb9e8daba4521380d9
Bryanmankind <shegeemankind@gmail.com>
2024-08-07 22:38:53 +0100

    webpage

--- src/Give.js
+++ src/Give.js
@@ -26,7 +26,7 @@ export function Give () {
 
         const fwConfig = {
             ...config,
-            text: 'Pay with Flutterwave!',
+            text: 'Give',
             callback: (response) => {
                console.log(response);
               closePaymentModal() 
@@ -36,18 +36,19 @@ export function Give () {
 
     return (
         <div clasName="give">
-            <div class="sec1">
+            <div className="sec1">
                 <img src="https://cdn.pixabay.com/photo/2019/10/24/14/26/hand-4574474_1280.jpg" alt=""/>
                 <h2>PARTNER WITH US</h2>
             </div>
-            <div class="sec2">
+            <div className="sec2">
                 <h2>About Giving </h2>
                 <p> Giving is a powerful way to make a significant difference in the world for Christ. 
                     By joining our monthly partner program, you can help us extend our reach, support vital projects, and bring hope to those in need. 
                     Learn more about how you can become a part of this transformative mission below.</p>
             </div>
-            <div class="sec3">
+            <div className="sec3">
                 <h2>Become a Monthly Partner or Make a One-Time Donation</h2>
+                
                 <FlutterWaveButton {...fwConfig} />
             </div>
         </div>
