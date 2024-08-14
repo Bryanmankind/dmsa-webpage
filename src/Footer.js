5e734ff6a71da199966ef0dba7fe63c0d5cc78b4
Bryanmankind <shegeemankind@gmail.com>
2024-08-15 00:03:04 +0100

    webpage

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';
+
+
 export function Footer({ togglePrayerRequest, togglePlanToVisit }) {
+  const [overlayVisible, setOverlayVisible] = useState(false);
+  const handleTogglePrayerRequest = () => {
+    togglePrayerRequest();
+    setOverlayVisible(true);
+  };
+  const handleTogglePlanToVisit = () => {
+    togglePlanToVisit();
+    setOverlayVisible(true);
+  };
+
+  const handleCloseOverlay = () => {
+    setOverlayVisible(false);
+  };
   return (
-    <div class="footer">
-      <button id="request" onClick={togglePrayerRequest}>Prayer Request</button>
-      <button id="visit" onClick={togglePlanToVisit}>Plan To Visit</button>
+    <>
+        <div
+        id="overlay"
+        className={overlayVisible ? 'show' : ''}
+        onClick={handleCloseOverlay}
+      ></div>
+    <div className="footer">
+      <button id="request" onClick={handleTogglePrayerRequest}>Prayer Request</button>
+      <button id="visit" onClick={handleTogglePlanToVisit}>Plan To Visit</button>
     </div>
+  </>
   );
 }
